075de50419b13d8e7c5d0ba58a1e0f5b7e38e58f
Justin Fernando <justinlenardf@gmail.com>
2025-06-17 21:26:10 +0800

    updated comment

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api", // ← update this to match your backend port
+  baseURL: "http://localhost:3001/api", // update to backend port
 });
 
 export default api;
